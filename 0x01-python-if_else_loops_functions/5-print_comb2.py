881176ec3649fe5c99e96652430d82e0e6b8d571
JosephMamani <jmmollenedo@gmail.com>
2022-04-26 23:43:45 +0000

    that prints numbers from 0 to 99 completing 0 besides from 1 to 9

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#for num in range(0, 99):
+#fr num in range(0, 99):
 #prnt("{}, ".format(str(num).zfill(2)), end='')
 for num in range(0, 99):
     print(str(num).rjust(2, '0'), end=', ')
