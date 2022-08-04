9bf6bffc83c6c691c18c6d15616f996c9bf44458
JosephMamani <jmmollenedo@gmail.com>
2022-08-04 04:46:32 +0000

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const number = parseInt(argv[2], 10);
-
-if (!parseInt(number)) {
-  console.log('Missing size');
-} else {
-  for (let step = 0; step < number; step++) {
-    console.log('X'.repeat(number));
-  }
+const num = parseInt(process.argv[2]);
+if (isNaN(num)) console.log('Missing size');
+else {
+  let v = '';
+  for (let i = 0; i < num; i++) v += 'X';
+  v.split('').forEach(() => console.log(v));
 }
