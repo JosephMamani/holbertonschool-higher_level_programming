e709385e88b8fb92a0e81de00eafcae79dc08798
JosephMamani <jmmollenedo@gmail.com>
2022-08-04 03:28:04 +0000

    script that prints two arguments passed to it, in the following format: “ is ”

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('process');
-let a = 'undefined';
-let b = 'undefined';
-if (argv[2]) {
-  a = argv[2];
-}
-if (argv[3]) {
-  b = argv[3];
-}
-console.log(a, 'is', b);
+const argv = process.argv.splice(2);
+console.log(argv[0] + ' is ' + argv[1]);
